62e68602df483992b480dc7f511d2743ed6fdabb
kangel <132186966+kangeluwu@users.noreply.github.com>
2024-02-22 22:26:22 +0800

    更新 MainMenuStateBackup.hx

--- source/MainMenuStateBackup.hx
+++ source/MainMenuStateBackup.hx
@@ -30,8 +30,8 @@ using StringTools;
 #elseif (hxCodec == "2.6.0") import VideoHandler as FlxVideo;
 #else import vlc.VideoHandler as FlxVideo; #end
 #else
-import PluginManger.FlxVideo as FlxVideo;
-import PluginManger.FlxVideoSprite;
+import PluginManager.FlxVideo as FlxVideo;
+import PluginManager.FlxVideoSprite;
 #end
 #end
 import flixel.util.FlxTimer;
