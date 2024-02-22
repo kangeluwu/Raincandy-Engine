c5f7e54e71dc854e428931b08477009850800cce
kangel <132186966+kangeluwu@users.noreply.github.com>
2024-02-22 21:59:02 +0800

    更新 CustomState.hx

--- source/CustomState.hx
+++ source/CustomState.hx
@@ -49,11 +49,14 @@ import flixel.util.FlxAxes;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
 #if VIDEOS_ALLOWED
+#if !ios
 #if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as FlxVideo;
 #elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as FlxVideo;
 #elseif (hxCodec == "2.6.0") import VideoHandler as FlxVideo;
 #else import vlc.VideoHandler as FlxVideo; #end
-#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideoSprite; #end
+#else
+import PluginManger.FlxVideo as FlxVideo;
+import PluginManger.FlxVideoSprite;
 #end
 import Sys;
 import sys.FileSystem;
