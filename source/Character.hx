991c85a5cf9f194e0888f6274e2282247e683b0c
kangel <132186966+kangeluwu@users.noreply.github.com>
2024-02-22 21:58:24 +0800

    更新 Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -35,12 +35,14 @@ import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 #if VIDEOS_ALLOWED
-
+#if !ios
 #if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as FlxVideo;
 #elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as FlxVideo;
 #elseif (hxCodec == "2.6.0") import VideoHandler as FlxVideo;
 #else import vlc.VideoHandler as FlxVideo; #end
-
+#else
+import PluginManger.FlxVideo as FlxVideo;
+import PluginManger.FlxVideoSprite;
 #end
 typedef CharacterFile = {
 	var crossColor:Null<FlxColor>;
